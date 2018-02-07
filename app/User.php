24aa7a0087e0fbb03e2067cf36d0112f72254163
Diego Matias <dm.diego.bh@gmail.com>
2018-02-07 11:12:40 -0200

    Now the user can login with e-mail or cpf

--- app/User.php
+++ app/User.php
@@ -29,7 +29,7 @@ class User extends Authenticatable
     ];
 
     public function findForPassport($username) {
-        return $this->where('CPF', $username)->first();
+        return $this->orWhere('CPF', $username)->orWhere('email',$username)->first();
     }
 
     public function role()
