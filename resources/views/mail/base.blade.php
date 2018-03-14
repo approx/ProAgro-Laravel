39a2ff8818acb83bcee4ea6be47af84ae36508cc
Diego Matias <dm.diego.bh@gmail.com>
2018-03-14 16:22:50 -0300

    Give acess user email template finished

--- resources/views/mail/base.blade.php
+++ resources/views/mail/base.blade.php
@@ -4,11 +4,43 @@
     </head>
     <style media="screen">
       .container{
-        max-width: 1024px;margin: auto;
+        position: relative;
+        max-width: 768px;margin: auto;
+        height: 100%;
+        background-color: #f7f5f2f2;
+        padding: 8px;
+        box-sizing: border-box;
+        color: #58bd2e;
+        text-align: center;
+      }
+      .logo{
+        width: 30%;
+        margin: 10px auto;
+      }
+      .hr{
+        min-height: 2px;
+        background-color: #4a8233;
+        margin: 2px 0px;
+      }
+      .bottom{
+        position: absolute;
+        left: 8px;
+        right: 8px;
+        bottom: 80px;
+      }
+      body{
+        margin: 0px;
+        background-image: url("{{ URL::to('/') }}/public/images/bg01.jpg");
       }
     </style>
+    @yield('style')
     <body>
         <div class="container">
+          <div class="logo">
+            <img width="100%" src="{{URL::to('/')}}/public/images/logo.png" alt="">
+          </div>
+          <div class="hr"></div>
+          <div class="hr bottom"></div>
             @yield('content')
         </div>
     </body>
--- resources/views/mail/registerUser.blade.php
+++ resources/views/mail/registerUser.blade.php
@@ -2,6 +2,24 @@
 
 @section('title','Pro Agro')
 
+@section('style')
+  <style media="screen">
+    .button{
+      position: absolute;
+      left: 50%;
+      transform: translate(-50%,50%);
+      bottom: 83px;
+      background-color: #4a8233;
+      border-radius: 16px;
+      color: white;
+      text-decoration: none;
+      padding: 8px;
+    }
+  </style>
+@endsection
+
 @section('content')
-  <p>teste</p>
+  <h1>Olá {{$name}}</h1>
+  <h3>Você acaba de receber acesso ao sistema da ProAgro, para que você possa acessar o nosso sistema primeiro você vai precisar de uma senha de acesso ao clicar no botão de cadastrar a senha você sera redirecionado a uma pagina para que sua senha seja cadastrada.</h3>
+  <a href="#" class="button">Cadastrar senha</a>
 @endsection
