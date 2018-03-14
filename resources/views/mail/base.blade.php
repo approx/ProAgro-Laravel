96b6f337b866934fc48a389e28f27d12cb1df6b3
Diego Matias <dm.diego.bh@gmail.com>
2018-03-14 14:35:03 -0300

    Created base for register user email

--- resources/views/mail/base.blade.php
+++ resources/views/mail/base.blade.php
@@ -2,6 +2,11 @@
     <head>
         <title>@yield('title')</title>
     </head>
+    <style media="screen">
+      .container{
+        max-width: 1024px;margin: auto;
+      }
+    </style>
     <body>
         <div class="container">
             @yield('content')
--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,9 @@
 // Route::get('/', function () {
 //     return view('welcome');
 // });
+
+Route::get('/register-user-email', function () {
+
+  return new App\Mail\GiveAcessUser();
+
+ });
