4a4098172fca216d7d517922d0ab6b618ff680bc
Diego Matias <dm.diego.bh@gmail.com>
2018-04-19 10:41:27 -0300

    added soft delete to client

--- database/migrations/2017_12_06_171018_create_clients_table.php
+++ database/migrations/2017_12_06_171018_create_clients_table.php
@@ -24,7 +24,7 @@ class CreateClientsTable extends Migration
             $table->integer('address_id');
             $table->integer('user_id');
             $table->integer('client_user')->nullable();
-            $table->boolean('active')->default(false);
+            $table->softDeletes();
             $table->timestamps();
         });
     }
