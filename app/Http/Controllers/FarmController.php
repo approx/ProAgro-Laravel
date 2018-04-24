00bec8992056c9f1c75fa18b8f8d02d757d63c08
Diego Matias <dm.diego.bh@gmail.com>
2018-04-24 09:59:24 -0300

    Start to implement stock

--- app/Http/Controllers/FarmController.php
+++ app/Http/Controllers/FarmController.php
@@ -4,13 +4,14 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Farm;
+use App\Stock;
 use Illuminate\Support\Facades\Auth;
 
 class FarmController extends Controller
 {
   public function index()
   {
-    $farms = Farm::with(['cultures','fields','client.address','client.user','inventory_itens','income_histories'])->orderBy('name')->get();
+    $farms = Farm::with(['cultures','fields','client.address','client.user','inventory_itens','income_histories','stocks'])->orderBy('name')->get();
     $filtered = $farms->filter(function ($value,$key) {
       return $value->client->user->id === Auth::id() || Auth::user()->role->name=='master';
     });
@@ -27,12 +28,27 @@ class FarmController extends Controller
     return $farm;
   }
 
+  public function createStock(Farm $farm)
+  {
+    if($farm->client->user->id!=Auth::id() && Auth::user()->role->name!='master') return response('you dont have access to this farm',400);
+
+    request()->validate([
+      'activity_type_id'=>'required',
+      'quantity'=>'required|numeric',
+      'unity_value'=>'required',
+      'product_name'=>'required'
+    ]);
+
+    return Stock::create(request()->all()+['farm_id'=>$farm->id]);
+  }
+
   public function get(Farm $farm)
   {
     if($farm->client->user->id!=Auth::id() && Auth::user()->role->name!='master') return response('you dont have access to this farm',400);
     $farm->inventory_itens;
     $farm->cultures;
     $farm->fields;
+    $farm->stocks;
     $farm->income_histories;
     $farm->client->address;
     return $farm;
--- app/Http/Controllers/StockController.php
+++ app/Http/Controllers/StockController.php
@@ -4,7 +4,27 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+use App\Stock;
+use Illuminate\Support\Facades\Auth;
+
 class StockController extends Controller
 {
-    //
+    public function index()
+    {
+      $stocks = Stock::with(['farm.client.user:id','stock_histories','activity_type'])->get();
+      $filtered = $stocks->filter(function($value,$key){
+        return $value->farm->client->user->id === Auth::id() || Auth::user()->role->name=='master';
+      });
+
+      return $filtered->values();
+    }
+
+    public function get(Stock $stock)
+    {
+      if($stock->farm->client->user->id!=Auth::id() && Auth::user()->role->name!='master') return response('you dont have access to this activity',400);
+
+      $stock->stock_histories;
+
+      return $stock;
+    }
 }
